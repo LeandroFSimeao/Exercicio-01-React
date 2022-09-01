aad438e80fc2ce41a4e60dfbe8e9d9ceb2ef83d5
Leandro Simeao <leandrosimeao@yahoo.com.br>
2022-09-01 17:49:55 -0300

    First commit

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+
+const Header = () =>{
+    return(
+        <div className='header'>
+            <div className='logo'>Logo</div>
+            <div className='topics'>
+                <h5>Topic 1</h5>
+                <h5>Topic 2</h5>
+                <h5>Topic 3</h5>
+                <h5>Topic 4</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
